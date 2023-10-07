2e7514a0b66bd686692260fac3017a27e1266483
LealTapia <leal_agareoz@hotmail.com>
2023-10-06 22:41:39 -0600

    Plantilla Wedding-MVC

---
+++ Modelos/conexion.php
@@ -0,0 +1,9 @@
+<?php 
+    class Conexion{
+        static public function conectar(){
+            $link = new PDO("mysql:host=localhost:3307;dbname=web-4c", "soporte3c", "soporte3c");
+            $link -> exec("set names utf8");
+            return $link;
+        }
+    }
+?>
